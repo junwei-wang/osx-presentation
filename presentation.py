27199ccd41843aea7b726bfe32ba4bb40d088269
Renaud Blanch <blanch@imag.fr>
2012-03-07 13:19:02 +0100

    added links on presenter view

--- presentation.py
+++ presentation.py
@@ -39,45 +39,14 @@ app = NSApplication.sharedApplication()
 url = NSURL.fileURLWithPath_(path)
 pdf = PDFDocument.alloc().initWithURL_(url)
 
-page = pdf.pageAtIndex_(2)
-for annotation in page.annotations():
-	annotation_type = type(annotation)
-	if annotation_type == PDFAnnotationText:
-		print annotation.contents()
-		annotation.setShouldDisplay_(False)
-	elif annotation_type == PDFAnnotationLink:
-		continue
-		
-		action = annotation.mouseUpAction()
-		t = type(action)
-		if t == PDFActionGoTo:
-			print action.destination()
-		elif t == PDFActionURL:
-			print action.URL()
-		elif t == PDFActionNamed:
-			print action.name()
-#	kPDFActionNamedNone = 0, 
-#    kPDFActionNamedNextPage = 1, 
-#    kPDFActionNamedPreviousPage = 2, 
-#    kPDFActionNamedFirstPage = 3, 
-#    kPDFActionNamedLastPage = 4, 
-#    kPDFActionNamedGoBack = 5, 
-#    kPDFActionNamedGoForward = 6, 
-#    kPDFActionNamedGoToPage = 7, 
-#    kPDFActionNamedFind = 8, 
-#    kPDFActionNamedPrint = 9, 
-#    kPDFActionNamedZoomIn = 10, 
-#    kPDFActionNamedZoomOut = 11
-
-
 page_count = pdf.pageCount()
 
 notes = defaultdict(list)
 for page_number in range(page_count):
 	page = pdf.pageAtIndex_(page_number)
+	page.setDisplaysAnnotations_(False)
 	for annotation in page.annotations():
 		if type(annotation) == PDFAnnotationText:
-			annotation.setShouldDisplay_(False)
 			notes[page_number].append(annotation.contents())
 
 current_page = 0
@@ -125,20 +94,30 @@ class PresenterView(NSView):
 		NSRectFillUsingOperation(bounds, NSCompositeClear)
 
 		# current 
-		page = pdf.pageAtIndex_(current_page)
-		_, (w, h) = page.boundsForBox_(kPDFDisplayBoxCropBox)
+		self.page = pdf.pageAtIndex_(current_page)
+		_, (w, h) = self.page.boundsForBox_(kPDFDisplayBoxCropBox)
 		r = current_width/w
 	
-		transform = NSAffineTransform.transform()
-		transform.translateXBy_yBy_(margin, height/2.)
-		transform.scaleXBy_yBy_(r, r)
-		transform.translateXBy_yBy_(0., -h/2.)
-		transform.concat()
+		self.transform = NSAffineTransform.transform()
+		self.transform.translateXBy_yBy_(margin, height/2.)
+		self.transform.scaleXBy_yBy_(r, r)
+		self.transform.translateXBy_yBy_(0., -h/2.)
+		self.transform.concat()
 
-		page.drawWithBox_(kPDFDisplayBoxCropBox)
-		transform.invert()
-		transform.concat()
-	
+		self.page.drawWithBox_(kPDFDisplayBoxCropBox)
+		
+		# links
+		NSColor.blueColor().setFill()
+		for annotation in self.page.annotations():
+			annotation_type = type(annotation)
+			if annotation_type == PDFAnnotationLink:
+				NSFrameRectWithWidth(annotation.bounds(), .5)
+		self.resetCursorRects()
+
+		self.transform.invert()
+		self.transform.concat()
+		
+
 		# time
 		now = NSString.stringWithString_(time.strftime("%H:%M:%S"))
 		now.drawAtPoint_withAttributes_((margin, height-margin*2), {
@@ -179,6 +158,17 @@ class PresenterView(NSView):
 		page.drawWithBox_(kPDFDisplayBoxCropBox)
 		transform.invert()
 		transform.concat()
+		
+	
+	def resetCursorRects(self):
+		self.discardCursorRects()
+		for annotation in self.page.annotations():
+			annotation_type = type(annotation)
+			if annotation_type == PDFAnnotationLink:
+				origin, size = annotation.bounds()
+				rect = (self.transform.transformPoint_(origin),
+				        self.transform.transformSize_(size))
+				self.addCursorRect_cursor_(rect, NSCursor.pointingHandCursor())
 	
 	
 	def keyDown_(self, event):
@@ -194,6 +184,19 @@ class PresenterView(NSView):
 		
 		elif c == "q":
 			app.terminate_(self)
+	
+	
+	def mouseUp_(self, event):
+		point = self.transform.transformPoint_(event.locationInWindow())
+		annotation = self.page.annotationAtPoint_(point)
+		if annotation is None:
+			return
+
+		annotation_type = type(annotation)
+		if annotation_type == PDFAnnotationLink:
+			destination = annotation.destination()
+			if destination:
+				goto_page(pdf.indexForPage_(destination.page()))
 
 
 # presentation ###############################################################
