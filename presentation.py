ab9d0a60e7de9eddee04b5a5221fefb692375193
Renaud Blanch <blanch@imag.fr>
2013-12-02 17:46:23 +0100

    bumping version to 1.1

--- presentation.py
+++ presentation.py
@@ -27,7 +27,7 @@ from collections import defaultdict
 # constants and helpers ######################################################
 
 NAME = "Présentation"
-MAJOR, MINOR = 1, 0
+MAJOR, MINOR = 1, 1
 VERSION = "%s.%s" % (MAJOR, MINOR)
 HOME = "http://iihm.imag.fr/blanch/software/osx-presentation/"
 CREDITS = """
