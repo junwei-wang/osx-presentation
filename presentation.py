9356d827bb9f479e4597007e6c6e863e8e287d48
Renaud Blanch <blanch@imag.fr>
2020-04-09 14:11:58 +0200

    bumped to 2.0.2

--- presentation.py
+++ presentation.py
@@ -33,7 +33,7 @@ if sys.version_info[0] == 3:
 
 NAME = "Présentation"
 ID = "fr.imag.iihm.blanch.osx-presentation"
-MAJOR, MINOR, PATCH = 2, 0, 1
+MAJOR, MINOR, PATCH = 2, 0, 2
 VERSION = "%s.%s.%s" % (MAJOR, MINOR, PATCH)
 HOME = "http://iihm.imag.fr/blanch/software/osx-presentation/"
 COPYRIGHT = "Copyright © 2011-2020 Renaud Blanch"
